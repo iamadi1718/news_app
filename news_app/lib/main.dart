882b64d315073b6143c2c3aa6030c26970408394
iamadi1718 <145316214+iamadi1718@users.noreply.github.com>
2025-10-19 16:36:53 +0530

    Update main.dart

--- news_app/lib/main.dart
+++ news_app/lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+// Import this if using FlutterFire CLI setup
+// import 'firebase_options.dart';
+
 import 'package:news_app/controller/BottomNavigationController.dart';
 import 'package:news_app/controller/HomepageController.dart';
 import 'package:news_app/controller/ThemeController.dart';
 import 'package:news_app/pages/homepages/HomePage.dart';
-void main()=>runApp(
-  const MyApp(),
-);
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Basic initialization; for CLI setup, use:
+  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
@@ -18,15 +28,14 @@ class _MyAppState extends State<MyApp> {
   final ThemeController themeController = Get.put(ThemeController());
   @override
   Widget build(BuildContext context) {
-    
     return GetMaterialApp(
-      home:Homepagecontroller(),
+      home: Homepagecontroller(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(),
-          darkTheme: ThemeData.dark(),
-          themeMode: themeController.isDarkMode.value
-              ? ThemeMode.dark
-              : ThemeMode.light,
+      darkTheme: ThemeData.dark(),
+      themeMode: themeController.isDarkMode.value
+          ? ThemeMode.dark
+          : ThemeMode.light,
     );
   }
 }
